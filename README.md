78a27972298184f8723f9bfc72e579b44d91f97c
HarshaNalluru <sanallur@microsoft.com>
2021-11-03 15:08:12 -0700

    instructions to repro

--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 This sample uses the bundling process described at [azure-sdk-for-js/Bundling.md#parcel-with-typescript](https://github.com/Azure/azure-sdk-for-js/blob/main/documentation/Bundling.md#parcel-with-typescript)
 
 ### Steps to repro
-1. Install node
-2. Install npm
-3. Clone this project
-4. 
+
+1. Install node and npm
+2. Clone this project
+3. `npm install` in this folder to install all the dependencies
+4. Create `.env` file and populate it with `APPCONFIG_CONNECTION_STRING` in it as shown in `sample.env`
+5. Install parcel globally - `npm install -g parcel-bundler`
+6. Run `parcel index.html` from this folder
+7. Following these steps should result in the error below
+   ![image](https://user-images.githubusercontent.com/10452642/140199875-f6310586-6ecf-45cf-ba3a-31c809f1bb42.png)
--- index.ts
+++ index.ts
@@ -6,27 +6,37 @@
  * @azsdk-weight 100
  */
 import { AppConfigurationClient } from "@azure/app-configuration";
+import {
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-base";
+import { WebTracerProvider } from "@opentelemetry/sdk-trace-web";
+import { ZoneContextManager } from "@opentelemetry/context-zone";
 
 // Load the .env file if it exists
 import * as dotenv from "dotenv";
 dotenv.config();
 
-export async function main() {
-  console.log(`Running helloworld sample`);
+const provider = new WebTracerProvider();
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
+provider.register({
+  // Changing default contextManager to use ZoneContextManager - supports asynchronous operations - optional
+  contextManager: new ZoneContextManager(),
+});
+
+async function main() {
   // Set the following environment variable or edit the value on the following line.
   const connectionString =
     process.env["APPCONFIG_CONNECTION_STRING"] || "<connection string>";
+
   const client = new AppConfigurationClient(connectionString);
-
-  const greetingKey = `harshan-${Math.ceil(Math.random() * 1000 + 1000)}`;
-
-  // creating a new setting
-  console.log(`Adding in new setting ${greetingKey}`);
-  await client.addConfigurationSetting({ key: greetingKey, value: "Hello!" });
+  await client.addConfigurationSetting({
+    key: `key-${Math.ceil(Math.random() * 1000 + 1000)}`,
+    value: "Hello!",
+  });
 }
 
 main().catch((err) => {
   console.error("Failed to run sample:", err);
-  process.exit(1);
 });
